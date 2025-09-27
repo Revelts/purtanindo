6ae364ecfb9e40db032619c9dfaa4ed69d342b4c
Leynardo Yosef <leynardo.yosef@logkar.com>
2025-09-28 04:38:10 +0700

    Google Tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
 			<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet" />
             <meta name="theme-color" content="#ffac00" />
+			<meta name="google-site-verification" content="03JacJ3nukN95mePnMzpmEKUElF3XX8NFIiAFXEwhQE" />
 			</Head>
 			<body>
 				<Main />
